ea1b5c9519ebd38c1b830f7538a8920f692cbbe2
Ansuman <sunmoonfortune@gmail.com>
2019-08-25 15:44:31 +0200

    Committing the todo changes

---
+++ src/test/java/com/user/usermanagement/todo/service/TodoServiceTest.java
@@ -0,0 +1,16 @@
+package com.user.usermanagement.todo.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TodoServiceTest {
+
+    private TodoService todoService;
+
+    @Test
+    public void first_test() {
+        assertEquals(2, 2);
+    }
+    
+}
